9119955a87b8fda69d9e6145749703b30a566d5a
Dhanush Kamatam <kamatam.dhanush.ece23@itbhu.ac.in>
2025-06-21 17:44:14 +0530

    changes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  experimental: {
+   
+  },
+  images: {
+    domains: ["images.unsplash.com", "cdn.example.com"],
+  },
+  typescript: {
+    ignoreBuildErrors: false,
+  },
 };
 
 export default nextConfig;
